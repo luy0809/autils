dc6e81a84fc97214a220446bdda6b571c9a7658b
luyu <luyu@dayima.com>
2016-12-01 17:42:42 +0800

    init.

---
+++ src/main/java/com/luy0809/java/autils/date/DateUtil.java
@@ -0,0 +1,10 @@
+package com.luy0809.java.autils.date;
+
+/**
+ * Created by luyu on 16-12-1.
+ */
+public class DateUtil {
+    public static void main(String[] args) {
+        System.out.print("hello world");
+    }
+}
